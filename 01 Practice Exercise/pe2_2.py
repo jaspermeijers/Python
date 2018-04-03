391a33fb517993fc78c1a7341e11e6b435454386
jaspermeijers <36531073+jaspermeijers@users.noreply.github.com>
2018-04-03 11:01:08 +0200

    Delete pe2_2.py

--- 01 Practice Exercise/pe2_2.py
+++
@@ -1,9 +0,0 @@
-cijferICT=eval(input('Cijfer Geo-ICT?'))
-cijferDATA_R=eval(input('Cijver Geo_DATA Regine?'))
-cijferDATA_H=eval(input('Cijfer Geo_DATA Hiddo?'))
-cijferPRO=eval(input('Cijfer Geo_PRO?'))
-
-gemiddelde=str((cijferPRO+cijferDATA_H+cijferICT+cijferDATA_R)/4)
-beloning=str(cijferICT*30 +cijferDATA_R*30 +cijferDATA_H*30 +cijferPRO*30)
-
-print('Met cijfers (gemiddeld een '+gemiddelde + ') leveren een beloning van €' +beloning + ' op!')
