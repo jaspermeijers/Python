52bc646f0e8dbabaec41bbeec1be62c823fca562
jaspermeijers <36531073+jaspermeijers@users.noreply.github.com>
2018-04-03 11:01:24 +0200

    Delete pe2_4.py

--- 01 Practice Exercise/pe2_4.py
+++
@@ -1,7 +0,0 @@
-uurloon=eval(input('Wat is je uurloon?'))
-gewerkt=eval(input('Hoeveel uur heb je gewerkt?'))
-totaal_loon=str(uurloon*gewerkt)
-
-print(str(gewerkt) + ' uur werken levert ' +totaal_loon + ' op')
-
-
